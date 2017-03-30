0576d776416e444d692eb6b499c540f23c92eec9
Dmitri <dimi615@pisem.net>
2017-03-30 14:55:13 +0300

    Story 51: Force option added to ProResMaker.ps1

--- PowerShell/ProResMaker.ps1
+++ PowerShell/ProResMaker.ps1
@@ -3,7 +3,7 @@
 
 This script for ProRes making
 
-Version: 1.0.1
+Version: 1.0.2
 
 .DESCRIPTION
 
@@ -33,29 +33,34 @@ Description
 -----------
 Convert video file to ProRes422 LT with deinterlace
 
+.EXAMPLE
+
+ProResMaker.ps1 FileName -Mode Proxy -Force
+
+Description
+-----------
+Convert video file to ProRes422 Proxy PCM audio into MOV container
+
 #>
 
 ########################################################################
 # Created By: Dmitri G. (2017)
 ########################################################################
 
-# Turn on/off DEBUG reporting
-
-# DEBUG ON
-# $DebugPreference = "Continue"
-# DEBUG OFF
-# $DebugPreference = "SilentlyContinue"
-
-# Deinterlace by: -vf "yadif"
-
 param(
 [Parameter(Mandatory=$true)]
 [string]$SourceFile,
 [ValidateSet("Proxy","LT","Normal","HQ")]
 [string]$Mode="Normal",
-[switch]$Deinterlace
+[switch]$Deinterlace,
+[switch]$Force
 
 ) #end param
+
+process {
+
+# DEBUG ON
+# $DebugPreference = "Continue"
 
 ########################################################################
 # Variables SET
@@ -63,13 +68,20 @@ param(
 
 # The help message output
 If ($SourceFile.Equals("--help") -or $SourceFile.Equals("/?")) {
-  Write-Host "`n*** Создание ProRes видео файла из исходника ***`n" -ForegroundColor Yellow
-  Write-Host "Варианты:" -ForegroundColor Yellow
-  Write-Host "---------" -ForegroundColor Yellow
-  Write-Host "> ProResMaker.ps1 FileName -Mode Proxy -Deinterlace" -ForegroundColor Yellow
-  Write-Host "> ProResMaker.ps1 FileName -Mode LT -Deinterlace" -ForegroundColor Yellow
-  Write-Host "> ProResMaker.ps1 FileName -Mode Normal -Deinterlace" -ForegroundColor Yellow
-  Write-Host "> ProResMaker.ps1 FileName -Mode HQ -Deinterlace`n`a" -ForegroundColor Yellow
+  Write-Host "`n*** Создание ProRes422 видео файла из исходника ***`n" -ForegroundColor Yellow
+  Write-Host "  Варианты:" -ForegroundColor Yellow
+  Write-Host "  ---------" -ForegroundColor Yellow
+  Write-Host -NoNewline "> ProResMaker.ps1 FileName -Mode Proxy -Deinterlace" -ForegroundColor Yellow
+  Write-Host "  # Кодирует файл в ProRes422 Proxy c устранением чересстрочности" -ForegroundColor Cyan
+  Write-Host -NoNewline "> ProResMaker.ps1 FileName -Mode LT -Deinterlace" -ForegroundColor Yellow
+  Write-Host "  # Кодирует файл в ProRes422 LT c устранением чересстрочности" -ForegroundColor Cyan
+  Write-Host -NoNewline "> ProResMaker.ps1 FileName -Mode Normal -Deinterlace" -ForegroundColor Yellow
+  Write-Host "  # Кодирует файл в ProRes422 c устранением чересстрочности" -ForegroundColor Cyan
+  Write-Host -NoNewline "> ProResMaker.ps1 FileName -Mode HQ -Deinterlace" -ForegroundColor Yellow
+  Write-Host "  # Кодирует файл в ProRes422 HQ c устранением чересстрочности" -ForegroundColor Cyan
+  Write-Host -NoNewline "> ProResMaker.ps1 FileName -Mode Proxy -Force" -ForegroundColor Yellow
+  Write-Host "  # Кодирует файл в ProRes422 Proxy c PCM звуком в контейнер MOV" -ForegroundColor Cyan
+  Write-Host "`n`a"
   Break
 }
 
@@ -118,36 +130,57 @@ switch ($Mode)
 
 [string]$directory = [System.IO.Path]::GetDirectoryName($SourceFile)
 [string]$strippedFileName = [System.IO.Path]::GetFileNameWithoutExtension($SourceFile)
-[string]$extension = [System.IO.Path]::GetExtension($SourceFile)
+
+if ($Force) {
+  [string]$extension = ".mov"
+  [string]$AudioCodec = "pcm_s16le"
+} else {
+  [string]$extension = [System.IO.Path]::GetExtension($SourceFile)
+  [string]$AudioCodec = "copy"
+}
+
 [string]$newFileName = $strippedFileName + $Prefix + [DateTime]::Now.ToString(".yyMMdd") + $extension
 [string]$newFilePath = [System.IO.Path]::Combine($directory, $newFileName)
 
-Clear-Host
+
+########################################################################
+# Functions
+########################################################################
+
+function exception {
+
+  Write-Host "`nOops!!! Something go wrong...!!!`n`a" -ForegroundColor Red
+
+}
 
 ########################################################################
 # Program START HERE
 ########################################################################
 
+Clear-Host
+
 switch ($Deinterlace)
 {
     true
     {
-        Write-Host "`nКодирование файлов ProRes 422 + Deinterlace, ждите...`n" -ForegroundColor Magenta
-        $strSwitches = ("-c:v","prores","-vf","`"yadif`"","-c:a","copy","-profile:v",$NumMode)
+        Write-Host "`nКодирование файла ProRes 422 + Deinterlace, ждите...`n" -ForegroundColor Magenta
+        $strSwitches = ("-c:v","prores","-vf","`"yadif`"","-c:a",$AudioCodec,"-profile:v",$NumMode)
     }
 
     default
     {
-        Write-Host "`nКодирование файлов ProRes 422, ждите...`n" -ForegroundColor Magenta
-        $strSwitches = ("-c:v","prores","-c:a","copy","-profile:v",$NumMode)
+        Write-Host "`nКодирование файла ProRes 422, ждите...`n" -ForegroundColor Magenta
+        $strSwitches = ("-c:v","prores","-c:a",$AudioCodec,"-profile:v",$NumMode)
     }
 }
 
-# Debug
-# Write-Host "`nYour switchers are: $strSwitches`n" -ForegroundColor Yellow
-# Write-Host "`nYour output name is: $newFilePath`n" -ForegroundColor Yellow
+Write-Debug "Your switchers are: $strSwitches"
+Write-Debug "Your output name is: $newFilePath"
 
-& ffmpeg -i $SourceFile $strSwitches $newFilePath
+try {
+  & ffmpeg -i $SourceFile $strSwitches $newFilePath
+  } catch { exception }
 
 Write-Host "`nКодирование выполнено, Спасибо!`n`a" -ForegroundColor Green
 
+}
